7418384427d320ee63ce78f9c62aff66175baf47
Misael Calisaya <misael.calisaya@gmail.com>
2023-05-08 07:54:40 -0300

    start

---
+++ src/app/modules/home/home-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomePageComponent
+
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
